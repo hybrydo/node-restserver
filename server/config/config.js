7aa4c31a241ad2af4ae4429eff78c50d8a88f89b
hybrydo <hybrydo@gmail.com>
2018-09-09 23:07:59 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-// ====================
-// puerto
-// ====================
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:123456pP@ds149732.mlab.com:49732/cafe';
+}
+process.env.URLDB = urlDB;
